b5640047570400d5c63465f16f3b02e56e4dfa02
GoDoyeon <97godo@naver.com>
2022-03-12 22:50:25 +0900

    [210813] 짝수와 홀수

---
+++ programmers/12937.js
@@ -0,0 +1,8 @@
+// [210813] 짝수와 홀수
+
+function solution(num) {
+  return num % 2 === 0 ? 'Even' : 'Odd';
+}
+
+const num = 0;
+console.log(solution(num));
---
+++ programmers/12937_2.js
@@ -0,0 +1,7 @@
+// [210813] 짝수와 홀수 (다른 사람 코드)
+function solution(num) {
+  return num % 2 ? 'Odd' : 'Even';
+}
+
+const num = 0;
+console.log(solution(num));
